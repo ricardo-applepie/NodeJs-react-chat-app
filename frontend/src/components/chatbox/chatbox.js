11efe48be2a9d98420ddc28a698a3c17bcd82b34
ricardo-applepie <mbancho12@gmail.com>
2021-09-09 23:47:28 +0200

    created chatbox design

---
+++ frontend/src/components/chatbox/chatbox.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import chatIcon from "../../images/chat-icon.png";
+import SendIcon from '@material-ui/icons/Send';
+const Chatbox =()=>{
+    return(
+        <div className='chat'>
+            <div className='chat__top'>
+                <div className='chat-icon'>
+                    <img src={chatIcon}/>
+                </div>
+  
+                <div>
+                    <h1>Chat Bot </h1>
+                </div>
+                <div>
+                   X
+                </div>
+            </div>
+            <div className='chat__conversation'>
+                     
+            </div>
+            <div className='typing'>
+              <input type='text' placeholder='Please type in message'/>
+              <div className='send-icon'>
+                    <SendIcon/>
+              </div>
+            </div>
+            <div className='chat-bottom'>
+               .
+            </div>
+        </div>
+    )
+}
+
+export default Chatbox; 
