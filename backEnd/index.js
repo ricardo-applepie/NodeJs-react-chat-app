1b7ff2e0d3b1cd37ccb7b5a270de82c37d14fa77
ricardo-applepie <mbancho12@gmail.com>
2021-09-10 01:40:46 +0200

    enabled front end app to send message to server using socket io

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,11 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 4000
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require("socket.io");
 
+const io = new Server(server);
+app.use(cors());
+let totalConnectedUsers=0;
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+    res.send('<h1>ok</h1>');
+});
 
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+io.on('connection', (socket) => {
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+});
+
+server.listen(4000, () => {
+    console.log('listening on *:4000');
+});
